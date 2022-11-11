1f17f4a5c5e3ae4d6b698ee8e911e918fe54c5f0
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-11-11 09:40:51 +0100

    created objects array, changed function addTask

--- dist/app.js
+++ dist/app.js
@@ -2,11 +2,29 @@ var taskInputElement = document.querySelector("#taskName");
 var addTaskButtonElement = document.querySelector(".addTask");
 var tasksContainer = document.querySelector(".tasks");
 var tasks = [
-    "Wyjść z psem na spacer",
-    "Zrobić trening",
-    "Nauczyć się TS",
-    "Znaleźć pracę",
+    {
+        name: "Wyjść z psem na spacer",
+        done: false,
+    },
+    {
+        name: "Zrobić trening",
+        done: false,
+    },
+    {
+        name: "Nauczyć się TS",
+        done: false,
+    },
+    {
+        name: "Znaleźć pracę",
+        done: false,
+    },
 ];
+// const tasks: string[] = [
+//   "Wyjść z psem na spacer",
+//   "Zrobić trening",
+//   "Nauczyć się TS",
+//   "Znaleźć pracę",
+// ];
 var renderTasks = function () {
     tasksContainer.innerText = "";
     tasks.forEach(function (task, index) {
@@ -14,18 +32,18 @@ var renderTasks = function () {
         var taskId = "task-".concat(index);
         var taskInputElement = document.createElement("input");
         taskInputElement.type = "checkbox";
-        taskInputElement.name = task;
+        taskInputElement.name = task.name;
         taskInputElement.id = taskId;
         var taskLabelElement = document.createElement("label");
         taskLabelElement.setAttribute("for", taskId);
-        taskLabelElement.innerText = task;
+        taskLabelElement.innerText = task.name;
         taskElement.appendChild(taskInputElement);
         taskElement.appendChild(taskLabelElement);
         tasksContainer.appendChild(taskElement);
     });
 };
-var addTask = function (task) {
-    tasks.push(task);
+var addTask = function (taskName) {
+    tasks.push({ name: taskName, done: false });
 };
 addTaskButtonElement.addEventListener("click", function (event) {
     event.preventDefault();
--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,34 @@ const addTaskButtonElement: HTMLButtonElement =
   document.querySelector(".addTask");
 const tasksContainer: HTMLUListElement = document.querySelector(".tasks");
 
-const tasks: string[] = [
-  "Wyjść z psem na spacer",
-  "Zrobić trening",
-  "Nauczyć się TS",
-  "Znaleźć pracę",
+const tasks: {
+  name: string;
+  done: boolean;
+}[] = [
+  {
+    name: "Wyjść z psem na spacer",
+    done: false,
+  },
+  {
+    name: "Zrobić trening",
+    done: false,
+  },
+  {
+    name: "Nauczyć się TS",
+    done: false,
+  },
+  {
+    name: "Znaleźć pracę",
+    done: false,
+  },
 ];
+
+// const tasks: string[] = [
+//   "Wyjść z psem na spacer",
+//   "Zrobić trening",
+//   "Nauczyć się TS",
+//   "Znaleźć pracę",
+// ];
 
 const renderTasks = () => {
   tasksContainer.innerText = "";
@@ -18,12 +40,12 @@ const renderTasks = () => {
 
     const taskInputElement = document.createElement("input");
     taskInputElement.type = "checkbox";
-    taskInputElement.name = task;
+    taskInputElement.name = task.name;
     taskInputElement.id = taskId;
 
     const taskLabelElement = document.createElement("label");
     taskLabelElement.setAttribute("for", taskId);
-    taskLabelElement.innerText = task;
+    taskLabelElement.innerText = task.name;
 
     taskElement.appendChild(taskInputElement);
     taskElement.appendChild(taskLabelElement);
@@ -31,8 +53,8 @@ const renderTasks = () => {
   });
 };
 
-const addTask = (task: string) => {
-  tasks.push(task);
+const addTask = (taskName: string) => {
+  tasks.push({ name: taskName, done: false });
 };
 
 addTaskButtonElement.addEventListener("click", (event: Event) => {
