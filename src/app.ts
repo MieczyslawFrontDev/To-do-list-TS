943e9f107d9d6a10234b66e7846c95fe44d6c09a
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-11-11 08:58:01 +0100

    get elements, created tasks array and created functions renderTasks

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-console.log("test3");
+const taskInputElement: HTMLInputElement = document.querySelector("#taskName");
+const addTaskButtonElement: HTMLButtonElement =
+  document.querySelector(".addTask");
+const tasksContainer: HTMLUListElement = document.querySelector(".tasks");
+
+const tasks: string[] = [
+  "Wyjść z psem na spacer",
+  "Zrobić trening",
+  "Nauczyć się TS",
+  "Znaleźć pracę",
+];
+
+const renderTasks = () => {
+  tasks.forEach((task, index) => {
+    const taskElement = document.createElement("li");
+    const taskId = `task-${index}`;
+
+    const taskInputElement = document.createElement("input");
+    taskInputElement.type = "checkbox";
+    taskInputElement.name = task;
+    taskInputElement.id = taskId;
+
+    const taskLabelElement = document.createElement("label");
+    taskLabelElement.setAttribute("for", taskId);
+    taskLabelElement.innerText = task;
+
+    taskElement.appendChild(taskInputElement);
+    taskElement.appendChild(taskLabelElement);
+    tasksContainer.appendChild(taskElement);
+  });
+};
+
+renderTasks();
