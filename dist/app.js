5ffb8d558558c504ed79e78690e3e37d0e0b8e66
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-11-12 15:03:22 +0100

    import renderCategories and renderTasks func, used parameters in renderCategories and renderTasks func

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,5 @@
+import { renderCategories } from "./features/renderCategories.js";
+import { renderTasks } from "./features/renderTask.js";
 var taskInputElement = document.querySelector("#taskName");
 var addTaskButtonElement = document.querySelector(".addTask");
 var tasksContainer = document.querySelector(".tasks");
@@ -5,39 +7,39 @@ var categoriesContainer = document.querySelector(".categories");
 var categories = ["general", "work", "gym", "hobby"];
 var selectedCategory;
 var tasks = [
-  {
-    name: "Wyjść z psem na spacer",
-    done: false,
-    category: "general",
-  },
-  {
-    name: "Zrobić trening",
-    done: false,
-    category: "gym",
-  },
-  {
-    name: "Nauczyć się TS",
-    done: false,
-    category: "hobby",
-  },
-  {
-    name: "Znaleźć pracę",
-    done: false,
-    category: "work",
-  },
+    {
+        name: "Wyjść z psem na spacer",
+        done: false,
+        category: "general",
+    },
+    {
+        name: "Zrobić trening",
+        done: false,
+        category: "gym",
+    },
+    {
+        name: "Nauczyć się TS",
+        done: false,
+        category: "hobby",
+    },
+    {
+        name: "Znaleźć pracę",
+        done: false,
+        category: "work",
+    },
 ];
 var addTask = function (task) {
-  tasks.push(task);
+    tasks.push(task);
 };
 addTaskButtonElement.addEventListener("click", function (event) {
-  event.preventDefault();
-  addTask({
-    name: taskInputElement.value,
-    done: false,
-    category: selectedCategory,
-  });
-  renderTasks();
+    event.preventDefault();
+    addTask({
+        name: taskInputElement.value,
+        done: false,
+        category: selectedCategory,
+    });
+    renderTasks(tasksContainer, tasks);
 });
 addTask({ name: "asasdasdasd", done: true, category: "general" });
-renderCategories();
-renderTasks();
+renderCategories(categories, categoriesContainer, selectedCategory);
+renderTasks(tasksContainer, tasks);
