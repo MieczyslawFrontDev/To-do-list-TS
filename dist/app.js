2a607be4f9f051349cc215fb09976d7119ac1209
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-11-16 12:51:16 +0100

    file compile

--- dist/app.js
+++ dist/app.js
@@ -9,45 +9,43 @@ var categoriesContainer = document.querySelector(".categories");
 var message = document.querySelector(".message");
 var removeTasksButton = document.querySelector(".removeDone");
 var categories = [
-  Category.General,
-  Category.Gym,
-  Category.Hobby,
-  Category.Work,
+    Category.General,
+    Category.Gym,
+    Category.Hobby,
+    Category.Work,
 ];
 var selectedCategory;
 var tasks = [
-  new TaskClass("Wyjść z psem na spacer", false, Category.General),
-  new TaskClass("Zrobić trening", false, Category.Gym),
-  new TaskClass("Nauczyć się TS", false, Category.Hobby),
-  new TaskClass("Znaleźć pracę", false, Category.Work),
+    new TaskClass("Wyjść z psem na spacer", false, Category.General),
+    new TaskClass("Zrobić trening", false, Category.Gym),
+    new TaskClass("Nauczyć się TS", false, Category.Hobby),
+    new TaskClass("Znaleźć pracę", false, Category.Work),
 ];
 var addTask = function (task) {
-  if (!taskInputElement.value) {
-    message.style.display = "flex";
-    return;
-  }
-  tasks.push(task);
-  taskInputElement.value = "";
-  message.style.display = "none";
+    if (!taskInputElement.value) {
+        message.style.display = "flex";
+        return;
+    }
+    tasks.push(task);
+    taskInputElement.value = "";
+    message.style.display = "none";
 };
 var updateSelectedCategory = function (newCategory) {
-  selectedCategory = newCategory;
+    selectedCategory = newCategory;
 };
 addTaskButtonElement.addEventListener("click", function (event) {
-  event.preventDefault();
-  var newTask = new TaskClass(taskInputElement.value, false, selectedCategory);
-  addTask(newTask);
-  renderTasks(tasksContainer, tasks);
+    event.preventDefault();
+    var newTask = new TaskClass(taskInputElement.value, false, selectedCategory);
+    addTask(newTask);
+    renderTasks(tasksContainer, tasks);
 });
 var removeDoneTasks = function () {
-  tasks = tasks.filter(function (task) {
-    return !task.done;
-  });
+    tasks = tasks.filter(function (task) { return !task.done; });
 };
 removeTasksButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  removeDoneTasks();
-  renderTasks(tasksContainer, tasks);
+    event.preventDefault();
+    removeDoneTasks();
+    renderTasks(tasksContainer, tasks);
 });
 renderCategories(categories, categoriesContainer, updateSelectedCategory);
 renderTasks(tasksContainer, tasks);
